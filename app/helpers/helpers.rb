4d2908bdfdb30be4699879ac673b08ac736a63d4
James Ferguson <ncjamesferguson@gmail.com>
2018-02-19 16:13:43 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,11 @@
 class Helpers
+
+  def current_user
+    User.find_by(email: params["email"], password: params["password"])
+  end
+
+  def is_logged_in?
+    self.current_user.id = session[:id] ? true : false
+  end
+
 end
